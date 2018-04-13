e1606eb909f81b77574387a7216942c2bb2133c6
kbkk <sejntcik@gmail.com>
2018-04-13 15:19:10 +0200

    add react-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Home from "./pages/Home";
 
-ReactDOM.render(<div/>, document.getElementById('root'));
+const Root = () => (
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
+);
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 registerServiceWorker();
