c77acc54ec4959870f949f34b6806ba98fb644ba
kbkk <sejntcik@gmail.com>
2018-04-16 23:12:16 +0200

    improve apiFetch - serialize non-string body

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -1,14 +1,22 @@
 import config from "./config";
 
-const apiFetch = (url, args = {}) => {
+const apiFetch = (url, options = {}) => {
   url = config.API_URL + url;
 
-  args.headers = {
-    ...args.headers,
+  options.method = options.method || 'GET';
+  options.headers = {
+    ...options.headers,
     'Content-Type': 'application/json'
   };
 
-  return fetch(url, args);
+  if (!['GET', 'HEAD'].includes(options.method)) {
+    options.body = options.body || {};
+
+    if (typeof options.body !== 'string')
+      options.body = JSON.stringify(options.body);
+  }
+
+  return fetch(url, options);
 };
 
 export default apiFetch;
